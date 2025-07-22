d2a01d09db67546e9d847f37212e88e5c08f3899
DevelopiaBD <abuabir783@gmail.com>
2025-07-22 11:21:10 +0600

    updated backend separation

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ require("dotenv").config();
 const userRoute = require('./routes/user.route');
 const connectDB = require('./config/database');
 
-// https://devofficial.knowledgehut.online
+
 app.use(cors({
-  origin: "*",
+  origin: "https://devofficial.knowledgehut.online",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true
 }));
